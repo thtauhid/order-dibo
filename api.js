d89ca8001f6744d4a6d8c89911a8046f29512bc3
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-11-27 04:40:39 +0600

    getOrdersByUserId api implementation

--- api.js
+++ api.js
@@ -1,39 +1,52 @@
 const getItems = () => {
-  return fetch(
-    "https://order-dibo-api.netlify.app/.netlify/functions/getItems"
-  ).then((res) => {
-    return res.json()
-  })
+	return fetch(
+		'https://order-dibo-api.netlify.app/.netlify/functions/getItems'
+	).then((res) => {
+		return res.json()
+	})
 }
 
 const createOrder = (data) => {
-  return fetch(
-    "https://order-dibo-api.netlify.app/.netlify/functions/createOrder",
-    {
-      body: JSON.stringify(data),
-      method: "POST",
-    }
-  ).then((res) => {
-    return res.json()
-  })
+	return fetch(
+		'https://order-dibo-api.netlify.app/.netlify/functions/createOrder',
+		{
+			body: JSON.stringify(data),
+			method: 'POST',
+		}
+	).then((res) => {
+		return res.json()
+	})
 }
 
 const getSingleOrder = (id) => {
-  return fetch(
-    "https://order-dibo-api.netlify.app/.netlify/functions/getSingleOrder",
-    {
-      body: JSON.stringify(id),
-      method: "POST",
-    }
-  ).then((res) => {
-    return res.json()
-  })
+	return fetch(
+		'https://order-dibo-api.netlify.app/.netlify/functions/getSingleOrder',
+		{
+			body: JSON.stringify(id),
+			method: 'POST',
+		}
+	).then((res) => {
+		return res.json()
+	})
+}
+
+const getOrdersByUserId = (id) => {
+	return fetch(
+		'https://order-dibo-api.netlify.app/.netlify/functions/getOrdersByUserId',
+		{
+			body: JSON.stringify(id),
+			method: 'POST',
+		}
+	).then((res) => {
+		return res.json()
+	})
 }
 
 const api = {
-  getItems,
-  createOrder,
-  getSingleOrder,
+	getItems,
+	createOrder,
+	getSingleOrder,
+	getOrdersByUserId,
 }
 
 export default api
--- components/Track.js
+++ components/Track.js
@@ -14,24 +14,20 @@ import {
 import { PriceContext, CartItemsContext } from '../Contexts'
 import api from '../api'
 
-const Track = () => {
+const Track = ({ history }) => {
 	const { cartPrice, setCartPrice } = useContext(PriceContext)
 	const { cartItems, setCartItems } = useContext(CartItemsContext)
 
 	const [items, setItems] = useState([])
 
 	useEffect(() => {
-		api.getItems()
+		api.getOrdersByUserId(5)
 			.then((items) => {
 				const data = items.map((item) => {
 					const { id: key } = item.ref['@ref']
-					let { title, details, price, category } = item.data
 					return {
 						key,
-						title,
-						details,
-						price,
-						category,
+						...item.data,
 					}
 				})
 				setItems(data)
@@ -39,70 +35,47 @@ const Track = () => {
 			.catch((err) => console.log(err))
 	}, [])
 
-	const categories = () => {
-		const categories = items.map((item) => item.category)
-		return categories.filter((value, index) => {
-			return categories.indexOf(value) === index
-		})
-	}
-
-	const itemsByCategory = (category) => {
-		const itemsx = items
-			.filter((item) => item.category == category)
-			.map((item) => item)
-		return (
-			<View>
-				<H3>{category}</H3>
-				{itemsx.map((item) => {
-					return (
-						<ItemCard
-							title={item.title}
-							details={item.details}
-							price={item.price}
-							category={item.category}
-						/>
-					)
-				})}
-			</View>
-		)
-	}
-
-	const ItemCard = (item) => {
-		return (
-			<Card style={{ flex: 0 }}>
-				<CardItem>
-					<Left>
-						<Body>
-							<Text>{item.title}</Text>
-							<Text note>{item.details}</Text>
-						</Body>
-					</Left>
-				</CardItem>
-				<CardItem>
-					<Left>
-						<Text>Tk {item.price}</Text>
-					</Left>
-					<Right>
-						<Button
-							onPress={() => {
-								setCartPrice(cartPrice + item.price)
-								setCartItems((prevItems) => [
-									item,
-									...prevItems,
-								])
-							}}>
-							<Text>Add to cart</Text>
-						</Button>
-					</Right>
-				</CardItem>
-			</Card>
-		)
-	}
-
 	return (
 		<View>
-			{categories().map((category) => {
-				return itemsByCategory(category)
+			{items.map((item) => {
+				return (
+					<Card>
+						<CardItem>
+							<Left>
+								<Body>
+									<Text>Order ID</Text>
+									<Text note>{item.key}</Text>
+								</Body>
+							</Left>
+						</CardItem>
+						<CardItem>
+							<Left>
+								<Body>
+									<Text>Price</Text>
+									<Text note>Tk {item.cartPrice}</Text>
+								</Body>
+							</Left>
+						</CardItem>
+						<CardItem>
+							<Left>
+								<Body>
+									<Text>Status</Text>
+									<Text note>{item.update[0]}</Text>
+								</Body>
+							</Left>
+							<Right>
+								<Body>
+									<Button
+										onPress={() =>
+											history.push(`/track/${item.key}`)
+										}>
+										<Text>View Order</Text>
+									</Button>
+								</Body>
+							</Right>
+						</CardItem>
+					</Card>
+				)
 			})}
 		</View>
 	)
