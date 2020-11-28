80d9a05579cf0e0abe4bdd2ad776d7dea892dedd
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-11-28 20:21:57 +0600

    Fixing Scroll Issue

--- components/TrackById.js
+++ components/TrackById.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Card, CardItem, Text, Left, Body, View, H3 } from 'native-base'
+import {
+	Card,
+	CardItem,
+	Text,
+	Left,
+	Body,
+	H3,
+	Container,
+	Content,
+} from 'native-base'
 import api from '../api'
 import 'react-native-get-random-values'
 import { v4 as uuid } from 'uuid'
@@ -29,74 +38,76 @@ const TrackById = ({ route }) => {
 	}, [])
 
 	return (
-		<View>
-			<Card>
-				<H3>Order Details</H3>
-				<CardItem>
-					<Left>
-						<Body>
-							<Text>Order ID</Text>
-							<Text note>{id}</Text>
-						</Body>
-					</Left>
-				</CardItem>
-				<CardItem>
-					<Left>
-						<Body>
-							<Text>Price</Text>
-							<Text note>Tk {orderData.cartPrice}</Text>
-						</Body>
-					</Left>
-				</CardItem>
-				<CardItem>
-					<Left>
-						<Body>
-							<Text>Address</Text>
-							<Text note>{orderData.address}</Text>
-						</Body>
-					</Left>
-				</CardItem>
-				<CardItem>
-					<Left>
-						<Body>
-							<Text>Phone No</Text>
-							<Text note>{orderData.phoneNumber}</Text>
-						</Body>
-					</Left>
-				</CardItem>
-			</Card>
-			<Card>
-				<H3>Items Ordered</H3>
+		<Container>
+			<Content>
+				<Card>
+					<H3>Order Details</H3>
+					<CardItem>
+						<Left>
+							<Body>
+								<Text>Order ID</Text>
+								<Text note>{id}</Text>
+							</Body>
+						</Left>
+					</CardItem>
+					<CardItem>
+						<Left>
+							<Body>
+								<Text>Price</Text>
+								<Text note>Tk {orderData.cartPrice}</Text>
+							</Body>
+						</Left>
+					</CardItem>
+					<CardItem>
+						<Left>
+							<Body>
+								<Text>Address</Text>
+								<Text note>{orderData.address}</Text>
+							</Body>
+						</Left>
+					</CardItem>
+					<CardItem>
+						<Left>
+							<Body>
+								<Text>Phone No</Text>
+								<Text note>{orderData.phoneNumber}</Text>
+							</Body>
+						</Left>
+					</CardItem>
+				</Card>
+				<Card>
+					<H3>Items Ordered</H3>
 
-				{orderData.cartItems.map((item) => {
-					return (
-						<CardItem key={uuid()}>
-							<Left>
-								<Body>
-									<Text>{item.title}</Text>
-									<Text note>Tk {item.price}</Text>
-								</Body>
-							</Left>
-						</CardItem>
-					)
-				})}
-			</Card>
-			<Card>
-				<H3>Order Update</H3>
-				{orderData.update.map((item) => {
-					return (
-						<CardItem key={uuid()}>
-							<Left>
-								<Body>
-									<Text>{item}</Text>
-									{/* <Text note>12 AM</Text> */}
-								</Body>
-							</Left>
-						</CardItem>
-					)
-				})}
-			</Card>
-		</View>
+					{orderData.cartItems.map((item) => {
+						return (
+							<CardItem key={uuid()}>
+								<Left>
+									<Body>
+										<Text>{item.title}</Text>
+										<Text note>Tk {item.price}</Text>
+									</Body>
+								</Left>
+							</CardItem>
+						)
+					})}
+				</Card>
+				<Card>
+					<H3>Order Update</H3>
+					{orderData.update.map((item) => {
+						return (
+							<CardItem key={uuid()}>
+								<Left>
+									<Body>
+										<Text>{item}</Text>
+										{/* <Text note>12 AM</Text> */}
+									</Body>
+								</Left>
+							</CardItem>
+						)
+					})}
+				</Card>
+			</Content>
+		</Container>
 	)
 }
 
