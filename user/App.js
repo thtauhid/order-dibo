3e827a87364b1c798211ecd22b7126f4ab9777c7
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-12-15 01:41:32 +0600

    Firebase SignIn implementation

--- user/App.js
+++ user/App.js
@@ -1,27 +1,12 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
-import {
-	Card,
-	CardItem,
-	Text,
-	Button,
-	Body,
-	Input,
-	Item,
-	Container,
-	Content,
-	Left,
-	Right,
-} from 'native-base'
+
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
-import Constants from 'expo-constants'
 
 import ShopNav from './navigation/ShopNav'
 import OrdersNav from './navigation/OrdersNav'
 import ProfileNav from './navigation/ProfileNav'
-
-import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
@@ -30,20 +15,6 @@ const Drawer = createDrawerNavigator()
 
 const App = () => {
 	const [isReady, setIsReady] = useState(false)
-	const [phoneNumber, setPhoneNumber] = useState('')
-
-	const recaptchaVerifier = useRef(null)
-
-	const sendVerification = () => {
-		const phoneProvider = new firebase.auth.PhoneAuthProvider()
-		phoneProvider
-			.verifyPhoneNumber(phoneNumber, recaptchaVerifier.current)
-			.then(setVerificationId)
-	}
-
-	const onChangePhoneNumber = (text) => {
-		return setPhoneNumber(text)
-	}
 
 	const getFonts = () =>
 		Font.loadAsync({
@@ -53,32 +24,13 @@ const App = () => {
 
 	if (isReady) {
 		return (
-			<>
-				<Container>
-					<Content>
-						<FirebaseRecaptchaVerifierModal
-							ref={recaptchaVerifier}
-							firebaseConfig={Constants.manifest.extra.firebase}
-						/>
-
-						<Item regular>
-							<Input
-								placeholder='Phone Number'
-								onChangeText={onChangePhoneNumber}
-								// keyboardType='phone-pad'
-								// autoCompleteType='tel'
-							/>
-						</Item>
-					</Content>
-				</Container>
-			</>
-			// <NavigationContainer>
-			// 	<Drawer.Navigator>
-			// 		<Drawer.Screen name='Shop' component={ShopNav} />
-			// 		<Drawer.Screen name='Orders' component={OrdersNav} />
-			// 		<Drawer.Screen name='Profile' component={ProfileNav} />
-			// 	</Drawer.Navigator>
-			// </NavigationContainer>
+			<NavigationContainer>
+				<Drawer.Navigator>
+					<Drawer.Screen name='Shop' component={ShopNav} />
+					<Drawer.Screen name='Orders' component={OrdersNav} />
+					<Drawer.Screen name='Profile' component={ProfileNav} />
+				</Drawer.Navigator>
+			</NavigationContainer>
 		)
 	} else {
 		return (
--- user/navigation/ProfileNav.js
+++ user/navigation/ProfileNav.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import Profile from '../components/Profile'
+import SignIn from '../components/SignIn'
 const Stack = createStackNavigator()
 
 const ProfileNav = () => {
 	return (
 		<Stack.Navigator>
-			<Stack.Screen name='Profile' component={Profile} />
+			<Stack.Screen name='Profile' component={SignIn} />
 		</Stack.Navigator>
 	)
 }
